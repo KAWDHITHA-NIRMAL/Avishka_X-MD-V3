adec971a96a5e09732d6519e344688934c1efe2f
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2024-12-24 21:17:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2JlwmATD#zLegNqDof9o5a9R08pD-_eaz35j3zTtGQaV_BNEuhOY",
+SESSION_ID: process.env.SESSION_ID || "keByUTLS#mpAfQZOLNlFOtuUVep4Vj0SdeV6hEPav4CDARN4sqE4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/2c1i67f.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "┏━━━━━━━━━━━➤◍◉➤\n│ *🔰𝐇𝐞𝐲 𝐈'𝐦 𝐀𝐯𝐢𝐬𝐡𝐤𝐚_𝐗-𝐌𝐃 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n┗━━━━━━━━━━━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰❤️*\n┗━━━━━━━━━━\n━➤◍◉➤\n┏━━━━━━━━━━━➤◍◉➤\n│ *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐀𝐯𝐢𝐬𝐡𝐤𝐚_𝐗 𝐓𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐲|*\n┗━━━━━━━━━━━━━━━━━━━━\n*2024🤍*\n\n┏━━━━━━━━━━━➤◍◉➤\n*This bot is made by Avishka_X technology.This is made with using Ai.You can use this bot easily.New updates will coming*\n┗━━━━━━━━━━━━━━━━━━━━\n\n🔰❤️ *join with us.....* 🔰❤️\nhttps://chat.whatsapp.com/KjDrxhc9b3f7MwmaoraCPC",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
